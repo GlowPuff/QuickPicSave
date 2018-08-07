3fc3624cc89d486f6b4d0e7de31f8b7b8417e9d6
GlowPuff <glowpuffmobile@gmail.com>
2018-08-06 21:22:25 -0400

    moved background.js

--- background/background.js
+++ background/background.js
@@ -1,26 +1,26 @@
 browser.runtime.onMessage.addListener(
-    function( request, sender, sendResponse )
-    {
-        function onStartedDownload( id )
+        function( request, sender, sendResponse )
         {
-            //console.log( 'Started downloading ID: ' + id );
-        }
+            function onStartedDownload( id )
+            {
+                //console.log( 'Started downloading ID: ' + id );
 
-        function onFailed( error )
-        {
-            console.log( "Download failed: " + error );
-        }
 
-        //sendResponse( { response: "DOWNLOADING IMAGE" } );
+                function onFailed( error )
+                {
+                    console.log( "Download failed: " + error );
+                }
 
-        var downloading = browser.downloads.download(
-        {
-            url: request.url,
-            filename: request.filename,
-            conflictAction: 'uniquify',
-            saveAs: false,
-            incognito: true
-        } );
-        downloading.then( onStartedDownload, onFailed );
-    }
-);
+                //sendResponse( { response: "DOWNLOADING IMAGE" } );
+
+                var downloading = browser.downloads.download(
+                {
+                    url: request.url,
+                    filename: request.filename,
+                    conflictAction: 'uniquify',
+                    saveAs: false,
+                    incognito: true
+                } );
+                downloading.then( onStartedDownload, onFailed );
+            }
+        );
